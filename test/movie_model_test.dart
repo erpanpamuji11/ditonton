39ebaac145943870476c8d4b148f0997f7c88815
erpanpamuji <lee.pamji@gmail.com>
2022-05-28 15:41:21 +0700

    add test for codemagic error test

---
+++ test/movie_model_test.dart
@@ -0,0 +1,43 @@
+import 'package:core/data/models/movies/movie_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:movies/domain/entities/movies/movie.dart';
+
+// this test is to overcome the failed codemagicc test
+void main() {
+  const tMovieModel = MovieModel(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: const [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  test('should be a subclass of Movie entity', () async {
+    final result = tMovieModel.toEntity();
+    expect(result, tMovie);
+  });
+}
